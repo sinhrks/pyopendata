c07f3e1ce85220deefb489d2593ed34afc3e4fb7
sinhrks <sinhrks@gmail.com>
2014-08-23 19:54:19 +0900

    TST: Add CKANTest

--- pyopendata/tests/test_ckan.py
+++ pyopendata/tests/test_ckan.py
@@ -1,38 +1,113 @@
-import pyopendata as pyod
+# pylint: disable-msg=E1101,W0613,W0603
 
-print(pyod)
-import urllib2
-import urllib
-import json
-import pprint
+from pyopendata import CKANStore
+
+import pandas.util.testing as tm
 
 
+class TestCKANTestSite(tm.TestCase):
+    _url = 'http://demo.ckan.org/'
+
+    def setUp(self):
+        self.store = CKANStore(self._url)
+
+    def test_packages(self):
+        result = self.store.packages
+        self.assertTrue('adur_district_spending' in result)
+
+        package = self.store.get_package_from_id('adur_district_spending')
+        for r in package.resources:
+            if r.id == '04127ad5-77e5-4a08-9f40-12d3c383e460':
+                df = r.read()
+                self.assertEqual(df.shape, (564, 6))
+                return
+        raise ValueError('Unable to find test data for validation')
+
+    def test_groups(self):
+        result = self.store.groups
+        self.assertTrue('data-explorer' in result)
+
+        packages = self.store.get_packages_from_group('data-explorer')
+        self.assertTrue(len(packages) > 0)
+        for p in packages:
+            for r in p.resources:
+                if r.id == '04127ad5-77e5-4a08-9f40-12d3c383e460':
+                    df = r.read()
+                    self.assertEqual(df.shape, (564, 6))
+                    return
+        raise ValueError('Unable to find test data for validation')
+
+    def test_tags(self):
+        result = self.store.tags
+        self.assertTrue('gold' in result)
+
+        resources = self.store.get_resources_from_tag('gold')
+        self.assertTrue(len(resources) > 0)
+
+        resource_names = [r.name for r in resources]
+        expected = ['who-data', 'gold-prices']
+        self.assertTrue(resource_names, expected)
+
+
+class TestDATAGOV(tm.TestCase):
+    _url = 'http://catalog.data.gov/'
+
+    def setUp(self):
+        self.store = CKANStore(self._url)
+
+    def test_packages(self):
+        result = self.store.packages
+        result_names = [r.name for r in result]
+        self.assertTrue('consumer-complaint-database' in result_names)
+
+        package = self.store.get_package_from_id('consumer-complaint-database')
+        for r in package.resources:
+            # ToDo: Find more small data
+            if r.id == '8842d7a8-d34e-4ad6-b324-8f8cb3c5beef':
+                df = r.read()
+                self.assertEqual(df.shape, (278771, 14))
+                return
+        raise ValueError('Unable to find test data for validation')
+
+    def test_groups(self):
+        result = self.store.groups
+        self.assertTrue('businessusa4208' in result)
+        self.assertTrue('climate5434' in result)
+
+        # Can't work now
+        # packages = self.store.get_packages_from_group('climate5434')
+        # self.assertTrue(len(packages) > 0)
+        # for p in packages:
+        #     for r in p.resources:
+        #         if r.id == '04127ad5-77e5-4a08-9f40-12d3c383e460':
+        #             df = r.read()
+        #             self.assertEqual(df.shape, (564, 6))
+        #             return
+        # raise ValueError('Unable to find test data "04127ad5-77e5-4a08-9f40-12d3c383e460"')
+
+    def test_tags(self):
+        result = self.store.tags
+        self.assertTrue('bank-service' in result)
+
+        resources = self.store.get_resources_from_tag('bank-service')
+        self.assertTrue(len(resources) > 0)
+        for r in resources:
+            if r.id == '8842d7a8-d34e-4ad6-b324-8f8cb3c5beef':
+                # tested in self.test_packages()
+                return
+        raise ValueError('Unable to find test data for validation')
+
+
+"""
+class TestDATAGOVUK(tm.TestCase):
+    _url = 'http://demo.ckan.org/'
+
+    def setUp(self):
+        self.store = CKANStore(self._url)
+"""
 # cstore = pyod.base.CKANStore('http://catalog.data.gov/')
 
-cstore = pyod.base.CKANStore('http://demo.ckan.org/')
-print(cstore.packages)
-# print('groups', len(cstore.groups))
-# print('tags', len(cstore.tags))
-print(cstore.get_package_from_id('1234567890'))
-print(cstore.get_resources_from_tag('gold'))
-print(cstore.get_packages_from_group('data-explorer'))
 
-quit()
-
-for c in cstore.packages:
-    print(c)
-
-for c in cstore.packages:
-    try:
-        # df = c.read()
-        # print(df)
-        quit()
-    except Exception as e:
-        print(e)
-        pass
-"""
-request = urllib2.Request('http://catalog.data.gov/api/action/package_show?id={0}'.format('DOC-5570'))
-response = urllib2.urlopen(request)
-response_dict = json.loads(response.read())
-print(response_dict)
-"""
+if __name__ == '__main__':
+    import nose
+    nose.runmodule(argv=[__file__, '-vvs', '-x', '--pdb', '--pdb-failure'], exit=False)
