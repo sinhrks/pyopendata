185c712631415bf2c28d4a332e6efd619642306f
sinhrks <sinhrks@gmail.com>
2014-10-05 15:02:02 +0900

    DOC: Fix sphinxext 2

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -20,14 +20,6 @@ import os
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
 
-sys.path.insert(0, os.path.abspath('../sphinxext'))
-
-sys.path.extend([
-    os.path.join(os.path.dirname(__file__),
-                 '..', '../..',
-                 'sphinxext')
-])
-
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
@@ -36,8 +28,8 @@ sys.path.extend([
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['ipython_sphinxext.ipython_directive',
-              'ipython_sphinxext.ipython_console_highlighting']
+extensions = ['IPython.sphinxext.ipython_directive',
+              'IPython.sphinxext.ipython_console_highlighting']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
